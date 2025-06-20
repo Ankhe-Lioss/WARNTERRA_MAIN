60d647945f2d3ecbff0a86b32b1ef5ad36ff986d
salvationofadues <songhung619@gmail.com>
2025-06-20 19:23:02 +0700

    sua lai aoe

--- code/aoe_warning.py
+++ code/aoe_warning.py
@@ -1,5 +1,5 @@
 from aoe import *
-Spawn_aoe_dict={  '''aoe skill=[frame_number,life_time]'''
+Spawn_aoe_dict={  #aoe skill=[frame_number,life_time]d
     'Spawn_rupture':(3,500),
     'Spawn_darkmatter':(3,1000)
 }
@@ -11,8 +11,9 @@ class Spawn_aoe(pygame.sprite.Sprite):
         self.pos=pos
         self.user_atk=user_atk
         #pass from dic
-        self.frame_number=Spawn_aoe_dict[self.name][0]
-        self.lifetime=Spawn_aoe_dict[self.name][1]
+        self.stat=Spawn_aoe_dict[self.name]
+        self.frame_number=self.stat[0]
+        self.lifetime=self.stat[1]
         self.frame_index = 0
         self.frames=[]
         self.load_frame()
@@ -23,7 +24,7 @@ class Spawn_aoe(pygame.sprite.Sprite):
         self.image_rect = self.image.get_frect(center=pos)
 
     def load_frame(self):
-        for i in range(3):
+        for i in range(self.frame_number):
             surf=pygame.image.load(os.path.join('images', 'enviroment',f'{self.name}', f'{i}.png')).convert_alpha()
             self.frames.append(surf)
 
@@ -36,7 +37,7 @@ class Spawn_aoe(pygame.sprite.Sprite):
 class Spawn_rupture(Spawn_aoe):
     def __init__(self, pos, game, user_atk):
         self.name='Spawn_rupture'
-        super.__init__( pos, game, user_atk)
+        super().__init__( pos, game, user_atk)
     def spawn(self):
         if pygame.time.get_ticks() - self.spawn_time >= self.lifetime:
             self.kill()
@@ -44,7 +45,7 @@ class Spawn_rupture(Spawn_aoe):
 class Spawn_darkmatter(Spawn_aoe):
     def __init__(self, pos, game, user_atk):
         self.name='Spawn_darkmatter'
-        super.__init__( pos, game, user_atk)
+        super().__init__( pos, game, user_atk)
     def spawn(self):
         if pygame.time.get_ticks() - self.spawn_time >= self.lifetime:
             self.kill()

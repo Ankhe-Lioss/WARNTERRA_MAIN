f778e9ad92c7a66098fe63f3dc778716b5d10c23
salvationofadues <songhung619@gmail.com>
2025-06-26 23:02:59 +0700

    uytj me dien roi

--- code/groups.py
+++ code/groups.py
@@ -21,6 +21,11 @@ class AllSprites(pygame.sprite.Group):
         for sprite in self:
             if not hasattr(sprite, 'get_draw') or sprite.get_draw==False:
                 continue
+            if hasattr(sprite, 'visible') and sprite.visible == False:
+                continue
+            if not hasattr(sprite, 'image_rect') or sprite.image_rect is None:
+                continue
+
             sprite_type = getattr(sprite, 'type', None)
             if sprite_type == 'ground':
                 ground_sprites.append(sprite)
@@ -34,9 +39,8 @@ class AllSprites(pygame.sprite.Group):
                 object_sprites.append(sprite)
 
         for layer in [ground_sprites, floor_sprites, object_sprites, top_sprites]:
-            for sprite in sorted(layer, key=lambda sprite: sprite.image_rect.bottom if hasattr(sprite, 'image_rect') else sprite.rect.bottom):
-                if hasattr(sprite, 'visible') and sprite.visible==False:
-                    continue
+            for sprite in sorted(layer, key=lambda sprite: sprite.image_rect.bottom):
+
                 self.display_surface.blit(sprite.image, sprite.image_rect.topleft + self.offset)
                 if Show_image and not hasattr(sprite, 'type') :
                     image_rect_outline = sprite.image_rect.copy()
@@ -54,6 +58,7 @@ class AllSprites(pygame.sprite.Group):
                 continue
 
             sprite_offset = pygame.math.Vector2(sprite.image_rect.center) - pygame.math.Vector2(player.rect.center)
+            if sprite_offset.length_squared() > 4000*4000:  # or 1000, or whatever makes sense
+                continue
+            sprite.update(dt)
             sprite.get_draw = True if sprite_offset.length_squared() < 1000 * 1000 else False
-            if sprite_offset.length_squared() < 4000*4000:  # or 1000, or whatever makes sense
-                sprite.update(dt)
